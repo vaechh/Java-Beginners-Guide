85bee8270cd41ab70ff1b94cd803ad34c85c9721
vaechh <john.ermakov27@gmail.com>
2023-09-22 13:04:58 +0500

    12.1 - TrafficLightDemo

---
+++ src/Chapter_12/TrafficLightDemo.java
@@ -0,0 +1,83 @@
+package Chapter_12;
+
+public class TrafficLightDemo {
+    public static void main(String[] args) {
+        TrafficLightSimulator tl =
+                new TrafficLightSimulator(TrafficLightColor.GREEN);
+        Thread thrd = new Thread(tl);
+        thrd.start();
+        for(int i=0; i < 9; i++) {
+            System.out.println(tl.getColor());
+            tl.waitForChange();
+        }
+        tl.cancel();
+    }
+}
+class TrafficLightSimulator implements Runnable {
+    private TrafficLightColor tlc; // holds the traffic light color
+    private boolean stop = false; // set to true to stop the simulation
+    private boolean changed = false; // true when the light has changed
+    TrafficLightSimulator(TrafficLightColor init) {
+        tlc = init;
+    }
+    TrafficLightSimulator() {
+        tlc = TrafficLightColor.RED;
+    }
+    // Start up the light.
+    public void run() {
+        while(!stop) {
+            try {
+                switch(tlc) {
+                    case GREEN:
+                        Thread.sleep(10000); // green for 10 seconds
+                        break;
+                    case YELLOW:
+                        Thread.sleep(2000); // yellow for 2 seconds
+                        break;
+                    case RED:
+                        Thread.sleep(12000); // red for 12 seconds
+                        break;
+                }
+            } catch(InterruptedException exc) {
+                System.out.println(exc);
+            }
+            changeColor();
+        }
+    }
+    // Change color.
+    synchronized void changeColor() {
+        switch(tlc) {
+            case RED:
+                tlc = TrafficLightColor.GREEN;
+                break;
+            case YELLOW:
+                tlc = TrafficLightColor.RED;
+                break;
+            case GREEN:
+                tlc = TrafficLightColor.YELLOW;
+        }
+        changed = true;
+        notify(); // signal that the light has changed
+    }
+    // Wait until a light change occurs.
+    synchronized void waitForChange() {
+        try {
+            while(!changed)
+                wait(); // wait for light to change
+            changed = false;
+        } catch(InterruptedException exc) {
+            System.out.println(exc);
+        }
+    }
+    // Return current color.
+    synchronized TrafficLightColor getColor() {
+        return tlc;
+    }
+    // Stop the traffic light.
+    synchronized void cancel() {
+        stop = true;
+    }
+}
+enum TrafficLightColor {
+    RED, GREEN, YELLOW
+}
